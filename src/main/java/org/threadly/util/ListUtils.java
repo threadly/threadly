8685dc4e2716720e2388ca2c62efbbd212ef6794
Mike Jensen <jentfoo@gmail.com>
2013-05-05 18:02:40 -0600

    Added in the list utilities and some basic tests for them

---
+++ src/main/java/org/threadly/util/ListUtils.java
@@ -0,0 +1,122 @@
+package org.threadly.util;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.RandomAccess;
+import java.util.concurrent.Delayed;
+import java.util.concurrent.TimeUnit;
+
+public class ListUtils {
+  public static int getInsertionEndIndex(List<? extends Delayed> list, 
+                                 Delayed key) {
+    return getInsertionEndIndex(list, key, list instanceof RandomAccess);
+  }
+  
+  public static int getInsertionEndIndex(List<? extends Delayed> list, 
+                                         Delayed key, boolean randomAccessList) {
+    int searchResult = binarySearch(list, key, 
+                                    randomAccessList);
+    if (searchResult >= 0) {
+      long keyDelay = key.getDelay(TimeUnit.MILLISECONDS);
+      Iterator<? extends Delayed> it = list.listIterator(searchResult);
+      while (it.hasNext() && it.next().getDelay(TimeUnit.MILLISECONDS) == keyDelay) {
+        searchResult++;
+      }
+      return searchResult;
+    } else {
+      return Math.abs(searchResult) - 1;
+    }
+  }
+  
+  /**
+   * A faster binary search algorithm for sorting a list.
+   * This algorithm works by actually knowing the values 
+   * and making smart decisions about how far to jump in 
+   * the list based on those values.  Which is why this
+   * can not take in a comparable interface like Collections
+   * does.  This was adapted from code posted from this 
+   * blog post: http://ochafik.com/blog/?p=106
+   * 
+   * @param list to be searched through
+   * @param key delay value to search for
+   * @return index where found, or -(insertion point) - 1 if not found
+   */
+  public static int binarySearch(List<? extends Delayed> list, 
+                                 Delayed key) {
+    return binarySearch(list, key, 
+                        list instanceof RandomAccess);
+  }
+  
+  /**
+   * @param list to be searched through
+   * @param key delay value to search for
+   * @param randomAccessList boolean representing if optimizations should be done for list that have cheap random access
+   * @return index where found, or -(insertion point) - 1 if not found
+   */
+  public static int binarySearch(List<? extends Delayed> list, 
+                                 Delayed key, boolean randomAccessList) {
+    if (list.isEmpty()) {
+      return -1;
+    }
+    
+    final int absoluteMin = 0;
+    final int absoluteMax = list.size() - 1;
+    
+    long keyValue = key.getDelay(TimeUnit.MILLISECONDS);
+    int min = absoluteMin;
+    int max = absoluteMax;
+    long minVal = list.get(absoluteMin).getDelay(TimeUnit.MILLISECONDS);
+    long maxVal = list.get(absoluteMax).getDelay(TimeUnit.MILLISECONDS);
+    
+    int nPreviousSteps = 1;
+    while (true) {
+      if (keyValue <= minVal) {
+        return keyValue == minVal ? min : -1 - min;
+      } else if (keyValue >= maxVal) {
+        return keyValue == maxVal ? max : -2 - max;
+      }
+      
+      int pivot;
+      // A typical binarySearch algorithm uses pivot = (min + max) / 2.
+      // The pivot we use here tries to be smarter and to choose a pivot close to the expected location of the key.
+      // This reduces dramatically the number of steps needed to get to the key.
+      // However, it does not work well with a logaritmic distribution of values.
+      // When the key is not found quickly the smart way, we switch to the standard pivot.
+      if (nPreviousSteps > 5) {
+        pivot = (min + max) >> 1;
+        // stop increasing nPreviousSteps from now on
+      } else {
+        // We cannot do the following operations in int precision, because there might be overflows.
+        // using a float is better performing than using a long (even on 64bit)
+        pivot = min + (int)((keyValue - (float)minVal) / (maxVal - (float)minVal) * (max - min));
+        nPreviousSteps++;
+      }
+      
+      long pivotVal = list.get(pivot).getDelay(TimeUnit.MILLISECONDS);
+      
+      if (keyValue > pivotVal) {
+        min = pivot + 1;
+        if (min > absoluteMax) {
+          return absoluteMax + 1;
+        }
+        minVal = list.get(min).getDelay(TimeUnit.MILLISECONDS);
+        if (randomAccessList) {
+          max--;
+          maxVal = list.get(max).getDelay(TimeUnit.MILLISECONDS);
+        }
+      } else if (keyValue < pivotVal) {
+        max = pivot - 1;
+        if (max < absoluteMin) {
+          return absoluteMin;
+        }
+        maxVal = list.get(max).getDelay(TimeUnit.MILLISECONDS);
+        if (randomAccessList) {
+          min++;
+          minVal = list.get(min).getDelay(TimeUnit.MILLISECONDS);
+        }
+      } else {
+        return pivot;
+      }
+    }
+  }
+}
---
+++ src/test/java/org/threadly/util/ListUtilsTest.java
@@ -0,0 +1,170 @@
+package org.threadly.util;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.Delayed;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+
+public class ListUtilsTest {
+  private static final int BINARY_SEARCH_RANDOM_SIZE = 100;
+  private static final int INCREMENTAL_SEARCH_COUNT = 100;
+  
+  @Test
+  public void getInsertionEndIndexTest() {
+    List<TestDelayed> list = new ArrayList<TestDelayed>(10);
+
+    TestDelayed zero = new TestDelayed(0);
+    TestDelayed one = new TestDelayed(1);
+    TestDelayed ten = new TestDelayed(10);
+    assertTrue(ListUtils.getInsertionEndIndex(list, zero) == 0);
+    assertTrue(ListUtils.getInsertionEndIndex(list, one) == 0);
+    assertTrue(ListUtils.getInsertionEndIndex(list, ten) == 0);
+    
+    list.add(list.size(), zero);
+    assertTrue(ListUtils.getInsertionEndIndex(list, zero) == 1);
+    assertTrue(ListUtils.getInsertionEndIndex(list, one) == 1);
+    assertTrue(ListUtils.getInsertionEndIndex(list, ten) == 1);
+    
+    list.add(list.size(), zero);
+    assertTrue(ListUtils.getInsertionEndIndex(list, zero) == 2);
+    assertTrue(ListUtils.getInsertionEndIndex(list, one) == 2);
+    assertTrue(ListUtils.getInsertionEndIndex(list, ten) == 2);
+    
+    list.add(list.size(), ten);
+    assertTrue(ListUtils.getInsertionEndIndex(list, zero) == 2);
+    assertTrue(ListUtils.getInsertionEndIndex(list, one) == 2);
+    assertTrue(ListUtils.getInsertionEndIndex(list, ten) == 3);
+    
+    list.add(list.size(), ten);
+    assertTrue(ListUtils.getInsertionEndIndex(list, zero) == 2);
+    assertTrue(ListUtils.getInsertionEndIndex(list, one) == 2);
+    assertTrue(ListUtils.getInsertionEndIndex(list, ten) == 4);
+    
+    // do one insert in the middle
+    list.add(2, one);
+    assertTrue(ListUtils.getInsertionEndIndex(list, zero) == 2);
+    assertTrue(ListUtils.getInsertionEndIndex(list, one) == 3);
+    assertTrue(ListUtils.getInsertionEndIndex(list, ten) == 5);
+  }
+  
+  @Test
+  public void binarySearchTest() {
+    List<TestDelayed> list = new ArrayList<TestDelayed>(BINARY_SEARCH_RANDOM_SIZE + 
+                                                          INCREMENTAL_SEARCH_COUNT + 10);
+    
+    TestDelayed zero = new TestDelayed(0);
+    
+    // verify against an empty list
+    assertTrue(ListUtils.binarySearch(list, zero, false) == -1);
+    assertTrue(ListUtils.binarySearch(list, zero, true) == -1);
+    
+    // add one and verify both contains and missing
+    TestDelayed negativeOne = new TestDelayed(-1);
+    list.add(negativeOne);
+    
+    assertTrue(ListUtils.binarySearch(list, zero, false) == -2);
+    assertTrue(ListUtils.binarySearch(list, zero, true) == -2);
+    
+    assertTrue(ListUtils.binarySearch(list, negativeOne, false) == 0);
+    assertTrue(ListUtils.binarySearch(list, negativeOne, true) == 0);
+    
+    // add one more and verify both contains and missing
+    TestDelayed ten = new TestDelayed(10);
+    list.add(ten);
+    
+    assertTrue(ListUtils.binarySearch(list, zero, false) == -2);
+    assertTrue(ListUtils.binarySearch(list, zero, true) == -2);
+    
+    assertTrue(ListUtils.binarySearch(list, negativeOne, false) == 0);
+    assertTrue(ListUtils.binarySearch(list, negativeOne, true) == 0);
+    assertTrue(ListUtils.binarySearch(list, ten, false) == 1);
+    assertTrue(ListUtils.binarySearch(list, ten, true) == 1);
+    
+    // add a duplicate entry and verify both contains and missing
+    list.add(ten);
+    
+    assertTrue(ListUtils.binarySearch(list, zero, false) == -2);
+    assertTrue(ListUtils.binarySearch(list, zero, true) == -2);
+    
+    assertTrue(ListUtils.binarySearch(list, negativeOne, false) == 0);
+    assertTrue(ListUtils.binarySearch(list, negativeOne, true) == 0);
+    long index = ListUtils.binarySearch(list, ten, false);
+    assertTrue(index == 1 || index == 2);
+    index = ListUtils.binarySearch(list, ten, true);
+    assertTrue(index == 1 || index == 2);
+    
+    // start above previous tests
+    for (int i = 20; i < INCREMENTAL_SEARCH_COUNT; i++) {
+      TestDelayed testItem = new TestDelayed(i);
+      int bsResult = ListUtils.binarySearch(list, testItem, true);
+      assertTrue(bsResult < 0); // should not be in list yet
+      int insertionIndex;
+      if (bsResult < 0) {
+        insertionIndex = Math.abs(bsResult) - 1;
+      } else {
+        insertionIndex = bsResult;
+      }
+      list.add(insertionIndex, testItem);
+      assertTrue(ListUtils.binarySearch(list, testItem, true) == insertionIndex);
+    }
+    
+    Random random = new Random(System.currentTimeMillis());
+    for (int i = 0; i < BINARY_SEARCH_RANDOM_SIZE; i++) {
+      TestDelayed testItem = new TestDelayed(random.nextInt(BINARY_SEARCH_RANDOM_SIZE));
+      int bsResult = ListUtils.binarySearch(list, testItem, true);
+      int insertionIndex;
+      if (bsResult < 0) {
+        insertionIndex = Math.abs(bsResult) - 1;
+      } else {
+        insertionIndex = bsResult;
+      }
+      list.add(insertionIndex, testItem);
+      
+      int raSearch = ListUtils.binarySearch(list, testItem, true);
+      int nonraSearch = ListUtils.binarySearch(list, testItem, false);
+      assertTrue(raSearch >= 0);
+      assertTrue(nonraSearch >= 0);
+    }
+  }
+  
+  private class TestDelayed implements Delayed {
+    private final long delayInMs;
+    
+    private TestDelayed(long delayInMs) {
+      this.delayInMs = delayInMs;
+    }
+    
+    @Override
+    public int compareTo(Delayed o) {
+      if (this == o) {
+        return 0;
+      } else {
+        long thisDelay = this.getDelay(TimeUnit.MILLISECONDS);
+        long otherDelay = o.getDelay(TimeUnit.MILLISECONDS);
+        if (thisDelay == otherDelay) {
+          return 0;
+        } else if (thisDelay > otherDelay) {
+          return 1;
+        } else {
+          return -1;
+        }
+      }
+    }
+
+    @Override
+    public long getDelay(TimeUnit unit) {
+      return unit.convert(delayInMs, 
+                          TimeUnit.MILLISECONDS);
+    }
+    
+    @Override
+    public String toString() {
+      return "d:" + delayInMs;
+    }
+  }
+}
