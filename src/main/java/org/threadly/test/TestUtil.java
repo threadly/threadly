6b992156e5dfcc088ae42d168eb12e3fddc30705
Mike Jensen <jentfoo@gmail.com>
2013-05-06 21:41:22 -0600

    Improved the javadocs for this class

--- src/main/java/org/threadly/test/TestUtil.java
+++ src/main/java/org/threadly/test/TestUtil.java
@@ -1,9 +1,17 @@
 package org.threadly.test;
 
 /**
+ * Tools to be used in unit testing
+ * 
  * @author jent - Mike Jensen
  */
 public class TestUtil {
+  /**
+   * Since sleeps are sometimes necessary, this makes
+   * an easy way to ignore InterruptedException's,
+   * 
+   * @param time time in milliseconds to make the thread to sleep
+   */
   public static void sleep(long time) {
     try {
       Thread.sleep(time);
