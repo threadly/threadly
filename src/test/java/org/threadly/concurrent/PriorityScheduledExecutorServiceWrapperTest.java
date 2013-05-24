b9875351034eb47061c0d7bd90f16fdc4e811be0
Mike Jensen <jentfoo@gmail.com>
2013-05-23 18:23:30 -0600

    Called unit tests for other half of this class, though the calls should be trivial we can easily run them against the same unit test abstraction to verify nothing has regressed.

--- src/test/java/org/threadly/concurrent/PriorityScheduledExecutorServiceWrapperTest.java
+++ src/test/java/org/threadly/concurrent/PriorityScheduledExecutorServiceWrapperTest.java
@@ -1,86 +1,244 @@
 package org.threadly.concurrent;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeoutException;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
+import org.threadly.concurrent.SimpleSchedulerInterfaceTest.PrioritySchedulerFactory;
 
 @SuppressWarnings("javadoc")
 public class PriorityScheduledExecutorServiceWrapperTest {
-  PriorityScheduledExecutorServiceWrapper wrapper;
-  
-  @Before
-  public void setup() {
-    wrapper = new PriorityScheduledExecutorServiceWrapper(new PriorityScheduledExecutor(1000, 1000, 200));
-  }
-  
-  @After
-  public void tearDown() {
-    wrapper.shutdown();
-    wrapper = null;
-  }
+  private static final int THREAD_COUNT = 1000;
+  private static final int KEEP_ALIVE_TIME = 200;
   
   @Test
   public void isTerminatedTest() {
-    ScheduledExecutorServiceTest.isTerminatedTest(wrapper);
+    PriorityScheduledExecutor executor = new PriorityScheduledExecutor(THREAD_COUNT, THREAD_COUNT, 
+                                                                       KEEP_ALIVE_TIME);
+    try {
+      ScheduledExecutorService wrapper = new PriorityScheduledExecutorServiceWrapper(executor);
+      ScheduledExecutorServiceTest.isTerminatedTest(wrapper);
+    } finally {
+      executor.shutdown();
+    }
   }
   
   @Test
   public void awaitTerminationTest() throws InterruptedException {
-    ScheduledExecutorServiceTest.awaitTerminationTest(wrapper);
+    PriorityScheduledExecutor executor = new PriorityScheduledExecutor(THREAD_COUNT, THREAD_COUNT, 
+                                                                       KEEP_ALIVE_TIME);
+    try {
+      ScheduledExecutorService wrapper = new PriorityScheduledExecutorServiceWrapper(executor);
+      ScheduledExecutorServiceTest.awaitTerminationTest(wrapper);
+    } finally {
+      executor.shutdown();
+    }
   }
   
   @Test
   public void submitCallableTest() throws InterruptedException, 
                                           ExecutionException {
-    ScheduledExecutorServiceTest.submitCallableTest(wrapper);
+    PriorityScheduledExecutor executor = new PriorityScheduledExecutor(THREAD_COUNT, THREAD_COUNT, 
+                                                                       KEEP_ALIVE_TIME);
+    try {
+      ScheduledExecutorService wrapper = new PriorityScheduledExecutorServiceWrapper(executor);
+      ScheduledExecutorServiceTest.submitCallableTest(wrapper);
+    } finally {
+      executor.shutdown();
+    }
   }
   
   @Test (expected = TimeoutException.class)
   public void futureGetTimeoutFail() throws InterruptedException, 
                                             ExecutionException, 
                                             TimeoutException {
-    ScheduledExecutorServiceTest.futureGetTimeoutFail(wrapper);
+    PriorityScheduledExecutor executor = new PriorityScheduledExecutor(THREAD_COUNT, THREAD_COUNT, 
+                                                                       KEEP_ALIVE_TIME);
+    try {
+      ScheduledExecutorService wrapper = new PriorityScheduledExecutorServiceWrapper(executor);
+      ScheduledExecutorServiceTest.futureGetTimeoutFail(wrapper);
+    } finally {
+      executor.shutdown();
+    }
   }
   
   @Test (expected = ExecutionException.class)
   public void futureGetExecutionFail() throws InterruptedException, 
                                               ExecutionException {
-    ScheduledExecutorServiceTest.futureGetExecutionFail(wrapper);
+    PriorityScheduledExecutor executor = new PriorityScheduledExecutor(THREAD_COUNT, THREAD_COUNT, 
+                                                                       KEEP_ALIVE_TIME);
+    try {
+      ScheduledExecutorService wrapper = new PriorityScheduledExecutorServiceWrapper(executor);
+      ScheduledExecutorServiceTest.futureGetExecutionFail(wrapper);
+    } finally {
+      executor.shutdown();
+    }
   }
   
   @Test
   public void futureCancelTest() throws InterruptedException, 
                                         ExecutionException {
-    ScheduledExecutorServiceTest.futureCancelTest(wrapper);
+    PriorityScheduledExecutor executor = new PriorityScheduledExecutor(THREAD_COUNT, THREAD_COUNT, 
+                                                                       KEEP_ALIVE_TIME);
+    try {
+      ScheduledExecutorService wrapper = new PriorityScheduledExecutorServiceWrapper(executor);
+      ScheduledExecutorServiceTest.futureCancelTest(wrapper);
+    } finally {
+      executor.shutdown();
+    }
   }
   
   @Test
   public void scheduleRunnableTest() throws InterruptedException, 
                                             ExecutionException {
-    ScheduledExecutorServiceTest.scheduleRunnableTest(wrapper);
+    PriorityScheduledExecutor executor = new PriorityScheduledExecutor(THREAD_COUNT, THREAD_COUNT, 
+                                                                       KEEP_ALIVE_TIME);
+    try {
+      ScheduledExecutorService wrapper = new PriorityScheduledExecutorServiceWrapper(executor);
+      ScheduledExecutorServiceTest.scheduleRunnableTest(wrapper);
+    } finally {
+      executor.shutdown();
+    }
   }
   
   @Test
   public void scheduleCallableTest() throws InterruptedException, 
                                             ExecutionException {
-    ScheduledExecutorServiceTest.scheduleCallableTest(wrapper);
+    PriorityScheduledExecutor executor = new PriorityScheduledExecutor(THREAD_COUNT, THREAD_COUNT, 
+                                                                       KEEP_ALIVE_TIME);
+    try {
+      ScheduledExecutorService wrapper = new PriorityScheduledExecutorServiceWrapper(executor);
+      ScheduledExecutorServiceTest.scheduleCallableTest(wrapper);
+    } finally {
+      executor.shutdown();
+    }
   }
   
   @Test
   public void scheduleCallableCancelTest() {
-    ScheduledExecutorServiceTest.scheduleCallableCancelTest(wrapper);
+    PriorityScheduledExecutor executor = new PriorityScheduledExecutor(THREAD_COUNT, THREAD_COUNT, 
+                                                                       KEEP_ALIVE_TIME);
+    try {
+      ScheduledExecutorService wrapper = new PriorityScheduledExecutorServiceWrapper(executor);
+      ScheduledExecutorServiceTest.scheduleCallableCancelTest(wrapper);
+    } finally {
+      executor.shutdown();
+    }
   }
   
   @Test
   public void scheduleWithFixedDelayTest() {
-    ScheduledExecutorServiceTest.scheduleWithFixedDelayTest(wrapper);
+    PriorityScheduledExecutor executor = new PriorityScheduledExecutor(THREAD_COUNT, THREAD_COUNT, 
+                                                                       KEEP_ALIVE_TIME);
+    try {
+      ScheduledExecutorService wrapper = new PriorityScheduledExecutorServiceWrapper(executor);
+      ScheduledExecutorServiceTest.scheduleWithFixedDelayTest(wrapper);
+    } finally {
+      executor.shutdown();
+    }
   }
   
   @Test
   public void scheduleWithFixedDelayFail() {
-    ScheduledExecutorServiceTest.scheduleWithFixedDelayFail(wrapper);
+    PriorityScheduledExecutor executor = new PriorityScheduledExecutor(THREAD_COUNT, THREAD_COUNT, 
+                                                                       KEEP_ALIVE_TIME);
+    try {
+      ScheduledExecutorService wrapper = new PriorityScheduledExecutorServiceWrapper(executor);
+      ScheduledExecutorServiceTest.scheduleWithFixedDelayFail(wrapper);
+    } finally {
+      executor.shutdown();
+    }
+  }
+  
+  @Test
+  public void executionTest() {
+    SchedulerFactory sf = new SchedulerFactory();
+    
+    try {
+      SimpleSchedulerInterfaceTest.executionTest(sf);
+    } finally {
+      sf.shutdown();
+    }
+  }
+  
+  @Test (expected = IllegalArgumentException.class)
+  public void executeTestFail() {
+    SchedulerFactory sf = new SchedulerFactory();
+    
+    try {
+      SimpleSchedulerInterfaceTest.executeTestFail(sf);
+    } finally {
+      sf.shutdown();
+    }
+  }
+  
+  @Test
+  public void scheduleExecutionTest() {
+    SchedulerFactory sf = new SchedulerFactory();
+    
+    try {
+      SimpleSchedulerInterfaceTest.scheduleExecutionTest(sf);
+    } finally {
+      sf.shutdown();
+    }
+  }
+  
+  @Test
+  public void scheduleExecutionFail() {
+    SchedulerFactory sf = new SchedulerFactory();
+    
+    try {
+      SimpleSchedulerInterfaceTest.scheduleExecutionFail(sf);
+    } finally {
+      sf.shutdown();
+    }
+  }
+  
+  @Test
+  public void recurringExecutionTest() {
+    SchedulerFactory sf = new SchedulerFactory();
+    
+    try {
+      SimpleSchedulerInterfaceTest.recurringExecutionTest(sf);
+    } finally {
+      sf.shutdown();
+    }
+  }
+  
+  @Test
+  public void recurringExecutionFail() {
+    SchedulerFactory sf = new SchedulerFactory();
+    
+    try {
+      SimpleSchedulerInterfaceTest.recurringExecutionFail(sf);
+    } finally {
+      sf.shutdown();
+    }
+  }
+
+  private class SchedulerFactory implements PrioritySchedulerFactory {
+    private final List<PriorityScheduledExecutor> executors;
+    
+    private SchedulerFactory() {
+      executors = new LinkedList<PriorityScheduledExecutor>();
+    }
+    
+    @Override
+    public SimpleSchedulerInterface make(int poolSize) {
+      PriorityScheduledExecutor executor = new PriorityScheduledExecutor(poolSize, poolSize, 200);
+      executors.add(executor);
+      return new PriorityScheduledExecutorServiceWrapper(executor);
+    }
+    
+    private void shutdown() {
+      Iterator<PriorityScheduledExecutor> it = executors.iterator();
+      while (it.hasNext()) {
+        it.next().shutdown();
+      }
+    }
   }
 }
