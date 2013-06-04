684c27a097c4846d3c68fd5e383d8131078c4e9e
Mike Jensen <jentfoo@gmail.com>
2013-06-03 22:33:10 -0600

    Increase this time slightly

--- src/test/java/org/threadly/concurrent/TaskExecutorDistributorTest.java
+++ src/test/java/org/threadly/concurrent/TaskExecutorDistributorTest.java
@@ -94,7 +94,7 @@ public class TaskExecutorDistributorTest {
       public boolean get() {
         return ready;
       }
-    }.blockTillTrue();
+    }.blockTillTrue(15000);
 
     synchronized (testLock) {
       Iterator<TDRunnable> it = runs.iterator();
