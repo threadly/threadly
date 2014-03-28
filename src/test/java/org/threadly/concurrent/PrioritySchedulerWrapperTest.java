bc0f84d741e8ca2eab3168699be7ba791a7d7a64
Mike Jensen <mike.jensen@readytalk.com>
2014-03-28 14:32:04 -0600

    More unit test verification around this simple wrapper (but still worth having)

--- src/test/java/org/threadly/concurrent/PrioritySchedulerWrapperTest.java
+++ src/test/java/org/threadly/concurrent/PrioritySchedulerWrapperTest.java
@@ -2,9 +2,14 @@ package org.threadly.concurrent;
 
 import static org.junit.Assert.*;
 
+import java.util.concurrent.Callable;
+
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.threadly.concurrent.future.FutureUtils;
+import org.threadly.concurrent.future.ListenableFuture;
+import org.threadly.test.concurrent.TestRunnable;
 
 @SuppressWarnings("javadoc")
 public class PrioritySchedulerWrapperTest {
@@ -50,5 +55,248 @@ public class PrioritySchedulerWrapperTest {
   public void isShutdownTest() {
     PrioritySchedulerWrapper psw = new PrioritySchedulerWrapper(scheduler, TaskPriority.Low);
     assertEquals(scheduler.isShutdown(), psw.isShutdown());
+    
+    scheduler.shutdownNow();
+    assertEquals(scheduler.isShutdown(), psw.isShutdown());
+  }
+  
+  @Test
+  public void executeTest() {
+    TestPriorityScheduler testScheduler = new TestPriorityScheduler();
+    PrioritySchedulerWrapper psw = new PrioritySchedulerWrapper(testScheduler, TaskPriority.Low);
+    psw.execute(new TestRunnable());
+    assertTrue(testScheduler.executeCalled);
+    
+    // reset and try with priority
+    testScheduler.executeCalled = false;
+    psw.execute(new TestRunnable(), TaskPriority.High);
+    assertTrue(testScheduler.executeCalled);
+  }
+  
+  @Test
+  public void scheduleTest() {
+    TestPriorityScheduler testScheduler = new TestPriorityScheduler();
+    PrioritySchedulerWrapper psw = new PrioritySchedulerWrapper(testScheduler, TaskPriority.Low);
+    psw.schedule(new TestRunnable(), 10);
+    assertTrue(testScheduler.scheduleCalled);
+    
+    // reset and try with priority
+    testScheduler.scheduleCalled = false;
+    psw.schedule(new TestRunnable(), 10, TaskPriority.High);
+    assertTrue(testScheduler.scheduleCalled);
+  }
+  
+  @Test
+  public void submitRunnableTest() {
+    TestPriorityScheduler testScheduler = new TestPriorityScheduler();
+    PrioritySchedulerWrapper psw = new PrioritySchedulerWrapper(testScheduler, TaskPriority.Low);
+    psw.submit(new TestRunnable());
+    assertTrue(testScheduler.submitRunnableCalled);
+    
+    // reset and try with priority
+    testScheduler.submitRunnableCalled = false;
+    psw.submit(new TestRunnable(), TaskPriority.High);
+    assertTrue(testScheduler.submitRunnableCalled);
+  }
+  
+  @Test
+  public void submitRunnableWithResultTest() {
+    TestPriorityScheduler testScheduler = new TestPriorityScheduler();
+    PrioritySchedulerWrapper psw = new PrioritySchedulerWrapper(testScheduler, TaskPriority.Low);
+    psw.submit(new TestRunnable(), new Object());
+    assertTrue(testScheduler.submitRunnableResultCalled);
+    
+    // reset and try with priority
+    testScheduler.submitRunnableResultCalled = false;
+    psw.submit(new TestRunnable(), new Object(), TaskPriority.High);
+    assertTrue(testScheduler.submitRunnableResultCalled);
+  }
+  
+  @Test
+  public void submitCallableTest() {
+    TestPriorityScheduler testScheduler = new TestPriorityScheduler();
+    PrioritySchedulerWrapper psw = new PrioritySchedulerWrapper(testScheduler, TaskPriority.Low);
+    psw.submit(new TestCallable());
+    assertTrue(testScheduler.submitCallableCalled);
+    
+    // reset and try with priority
+    testScheduler.submitCallableCalled = false;
+    psw.submit(new TestCallable(), TaskPriority.High);
+    assertTrue(testScheduler.submitCallableCalled);
+  }
+  
+  @Test
+  public void submitScheduledRunnableTest() {
+    TestPriorityScheduler testScheduler = new TestPriorityScheduler();
+    PrioritySchedulerWrapper psw = new PrioritySchedulerWrapper(testScheduler, TaskPriority.Low);
+    psw.submitScheduled(new TestRunnable(), 10);
+    assertTrue(testScheduler.submitScheduledRunnableCalled);
+    
+    // reset and try with priority
+    testScheduler.submitScheduledRunnableCalled = false;
+    psw.submitScheduled(new TestRunnable(), 10, TaskPriority.High);
+    assertTrue(testScheduler.submitScheduledRunnableCalled);
+  }
+  
+  @Test
+  public void submitScheduledRunnableWithResultTest() {
+    TestPriorityScheduler testScheduler = new TestPriorityScheduler();
+    PrioritySchedulerWrapper psw = new PrioritySchedulerWrapper(testScheduler, TaskPriority.Low);
+    psw.submitScheduled(new TestRunnable(), new Object(), 10);
+    assertTrue(testScheduler.submitScheduledRunnableResultCalled);
+    
+    // reset and try with priority
+    testScheduler.submitScheduledRunnableResultCalled = false;
+    psw.submitScheduled(new TestRunnable(), new Object(), 10, TaskPriority.High);
+    assertTrue(testScheduler.submitScheduledRunnableResultCalled);
+  }
+  
+  @Test
+  public void submitScheduledCallableTest() {
+    TestPriorityScheduler testScheduler = new TestPriorityScheduler();
+    PrioritySchedulerWrapper psw = new PrioritySchedulerWrapper(testScheduler, TaskPriority.Low);
+    psw.submitScheduled(new TestCallable(), 10);
+    assertTrue(testScheduler.submitScheduledCallableCalled);
+    
+    // reset and try with priority
+    testScheduler.submitScheduledCallableCalled = false;
+    psw.submitScheduled(new TestCallable(), 10, TaskPriority.High);
+    assertTrue(testScheduler.submitScheduledCallableCalled);
+  }
+  
+  @Test
+  public void scheduleWithFixedDelayTest() {
+    TestPriorityScheduler testScheduler = new TestPriorityScheduler();
+    PrioritySchedulerWrapper psw = new PrioritySchedulerWrapper(testScheduler, TaskPriority.Low);
+    psw.scheduleWithFixedDelay(new TestRunnable(), 10, 10);
+    assertTrue(testScheduler.scheduleWithFixedDelayCalled);
+    
+    // reset and try with priority
+    testScheduler.scheduleWithFixedDelayCalled = false;
+    psw.scheduleWithFixedDelay(new TestRunnable(), 10, 10, TaskPriority.High);
+    assertTrue(testScheduler.scheduleWithFixedDelayCalled);
+  }
+  
+  // TODO - this may be good to move to something like mockito
+  private static class TestPriorityScheduler implements PrioritySchedulerInterface {
+    private boolean executeCalled = false;
+    private boolean scheduleCalled = false;
+    private boolean submitRunnableCalled = false;
+    private boolean submitRunnableResultCalled = false;
+    private boolean submitCallableCalled = false;
+    private boolean submitScheduledRunnableCalled = false;
+    private boolean submitScheduledRunnableResultCalled = false;
+    private boolean submitScheduledCallableCalled = false;
+    private boolean scheduleWithFixedDelayCalled = false;
+
+    @Override
+    public boolean isShutdown() {
+      return false;
+    }
+
+    @Override
+    public void execute(Runnable task, TaskPriority priority) {
+      executeCalled = true;
+    }
+
+    @Override
+    public ListenableFuture<?> submit(Runnable task, TaskPriority priority) {
+      submitRunnableCalled = true;
+      return FutureUtils.immediateFailureFuture(new UnsupportedOperationException());
+    }
+
+    @Override
+    public <T> ListenableFuture<T> submit(Runnable task, T result, TaskPriority priority) {
+      submitRunnableResultCalled = true;
+      return FutureUtils.immediateFailureFuture(new UnsupportedOperationException());
+    }
+
+    @Override
+    public <T> ListenableFuture<T> submit(Callable<T> task, TaskPriority priority) {
+      submitCallableCalled = true;
+      return FutureUtils.immediateFailureFuture(new UnsupportedOperationException());
+    }
+
+    @Override
+    public void schedule(Runnable task, long delayInMs, TaskPriority priority) {
+      scheduleCalled = true;
+    }
+
+    @Override
+    public ListenableFuture<?> submitScheduled(Runnable task, long delayInMs, TaskPriority priority) {
+      submitScheduledRunnableCalled = true;
+      return FutureUtils.immediateFailureFuture(new UnsupportedOperationException());
+    }
+
+    @Override
+    public <T> ListenableFuture<T> submitScheduled(Runnable task, T result, long delayInMs,
+                                                   TaskPriority priority) {
+      submitScheduledRunnableResultCalled = true;
+      return FutureUtils.immediateFailureFuture(new UnsupportedOperationException());
+    }
+
+    @Override
+    public <T> ListenableFuture<T> submitScheduled(Callable<T> task, long delayInMs,
+                                                   TaskPriority priority) {
+      submitScheduledCallableCalled = true;
+      return FutureUtils.immediateFailureFuture(new UnsupportedOperationException());
+    }
+
+    @Override
+    public void scheduleWithFixedDelay(Runnable task, long initialDelay, long recurringDelay,
+                                       TaskPriority priority) {
+      scheduleWithFixedDelayCalled = true;
+    }
+
+    @Override
+    public TaskPriority getDefaultPriority() {
+      throw new UnsupportedOperationException();
+    }
+    
+    // NO OPERATIONS WITHOUT PRIORITY SHOULD BE CALLED
+    @Override
+    public ListenableFuture<?> submitScheduled(Runnable task, long delayInMs) {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public <T> ListenableFuture<T> submitScheduled(Runnable task, T result, long delayInMs) {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public <T> ListenableFuture<T> submitScheduled(Callable<T> task, long delayInMs) {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void schedule(Runnable task, long delayInMs) {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void scheduleWithFixedDelay(Runnable task, long initialDelay, long recurringDelay) {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void execute(Runnable command) {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public ListenableFuture<?> submit(Runnable task) {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public <T> ListenableFuture<T> submit(Runnable task, T result) {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public <T> ListenableFuture<T> submit(Callable<T> task) {
+      throw new UnsupportedOperationException();
+    }
   }
 }
