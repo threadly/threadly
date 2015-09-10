fcb202bfc39213cb0350d00fab3201e46e9479a9
Mike Jensen <jentfoo@gmail.com>
2015-09-10 13:04:20 -0600

    Added wrappers for renaming at either a Runnable level, or pool level
    This resolves #170, adding the ability to in a more generic way rename tasks submitted from a specific executor/scheduler reference.

---
+++ src/test/java/org/threadly/concurrent/ThreadRenamingSchedulerServiceTest.java
@@ -0,0 +1,44 @@
+package org.threadly.concurrent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SuppressWarnings("javadoc")
+public class ThreadRenamingSchedulerServiceTest extends SchedulerServiceInterfaceTest {
+  @Override
+  protected SchedulerServiceFactory getSchedulerServiceFactory() {
+    return new ThreadRenamingPoolWrapperFactory();
+  }
+
+  private static class ThreadRenamingPoolWrapperFactory implements SchedulerServiceFactory {
+    private final List<PriorityScheduler> schedulers = new ArrayList<PriorityScheduler>(2);
+
+    @Override
+    public SubmitterExecutor makeSubmitterExecutor(int poolSize, boolean prestartIfAvailable) {
+      return makeSubmitterScheduler(poolSize, prestartIfAvailable);
+    }
+    
+    @Override
+    public SubmitterScheduler makeSubmitterScheduler(int poolSize, boolean prestartIfAvailable) {
+      return makeSchedulerService(poolSize, prestartIfAvailable);
+    }
+
+    @Override
+    public SchedulerService makeSchedulerService(int poolSize, boolean prestartIfAvailable) {
+      PriorityScheduler ps = new PriorityScheduler(poolSize);
+      if (prestartIfAvailable) {
+        ps.prestartAllThreads();
+      }
+      schedulers.add(ps);
+
+      return new ThreadRenamingSchedulerService(ps, "foo", false);
+    }
+
+    @Override
+    public void shutdown() {
+      for (PriorityScheduler ps : schedulers) {
+        ps.shutdownNow();
+      }
+    }
+  }
+}
