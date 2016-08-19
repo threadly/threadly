2cf237e3494be1bf836bafb202af11a8331791ab
Mike Jensen <jent@threadly.org>
2016-08-19 10:56:19 -0600

    Added `SchedulerExecutorDelegator`
    A way to combine an executor and scheduler, allowing different behavior or implementations depending on the action taking place.

---
+++ src/main/java/org/threadly/concurrent/wrapper/SchedulerExecutorDelegator.java
@@ -0,0 +1,55 @@
+package org.threadly.concurrent.wrapper;
+
+import org.threadly.concurrent.AbstractSubmitterScheduler;
+import org.threadly.concurrent.SubmitterExecutor;
+import org.threadly.concurrent.SubmitterScheduler;
+import org.threadly.util.ArgumentVerifier;
+
+/**
+ * <p>Class which takes in both an executor and a scheduler.  Delegating executions to the executor 
+ * when possible, and otherwise submitting to the provided scheduler.  This can be used to provide 
+ * different behavior/implementations between scheduled and executed tasks (for example you could 
+ * use {@link PrioritySchedulerDefaultPriorityWrapper} to have a different default priority for 
+ * scheduled tasks vs executed).</p>
+ * 
+ * @author jent - Mike Jensen
+ * @since 4.7.0
+ */
+public class SchedulerExecutorDelegator extends AbstractSubmitterScheduler {
+  protected final SubmitterExecutor parentExecutor;
+  protected final SubmitterScheduler parentScheduler;
+  
+  /**
+   * Constructs a new delegator with the provided pools to defer executions to.
+   * 
+   * @param parentExecutor Executor to use when ever possible
+   * @param parentScheduler Scheduler to use when executions need to be delayed
+   */
+  public SchedulerExecutorDelegator(SubmitterExecutor parentExecutor, 
+                                    SubmitterScheduler parentScheduler) {
+    ArgumentVerifier.assertNotNull(parentExecutor, "parentExecutor");
+    ArgumentVerifier.assertNotNull(parentScheduler, "parentScheduler");
+    
+    this.parentExecutor = parentExecutor;
+    this.parentScheduler = parentScheduler;
+  }
+  
+  @Override
+  public void scheduleWithFixedDelay(Runnable task, long initialDelay, long recurringDelay) {
+    parentScheduler.scheduleWithFixedDelay(task, initialDelay, recurringDelay);
+  }
+  
+  @Override
+  public void scheduleAtFixedRate(Runnable task, long initialDelay, long period) {
+    parentScheduler.scheduleAtFixedRate(task, initialDelay, period);
+  }
+  
+  @Override
+  protected void doSchedule(Runnable task, long delayInMillis) {
+    if (delayInMillis > 0) {
+      parentScheduler.schedule(task, delayInMillis);
+    } else {
+      parentExecutor.execute(task);
+    }
+  }
+}
---
+++ src/test/java/org/threadly/concurrent/wrapper/SchedulerExecutorDelegatorTest.java
@@ -0,0 +1,82 @@
+package org.threadly.concurrent.wrapper;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.threadly.concurrent.DoNothingRunnable;
+import org.threadly.concurrent.SubmitterExecutor;
+import org.threadly.concurrent.SubmitterScheduler;
+import org.threadly.concurrent.SubmitterSchedulerInterfaceTest;
+import org.threadly.concurrent.PrioritySchedulerTest.PrioritySchedulerFactory;
+import org.threadly.test.concurrent.TestableScheduler;
+
+@SuppressWarnings("javadoc")
+public class SchedulerExecutorDelegatorTest extends SubmitterSchedulerInterfaceTest {
+  private TestableScheduler executor;
+  private TestableScheduler scheduler;
+  private SchedulerExecutorDelegator delegator;
+  
+  @Before
+  public void setup() {
+    executor = new TestableScheduler();
+    scheduler = new TestableScheduler();
+    delegator = new SchedulerExecutorDelegator(executor, scheduler);
+  }
+  
+  @After
+  public void cleanup() {
+    executor = null;
+    scheduler = null;
+    delegator = null;
+  }
+
+  @Override
+  protected SubmitterSchedulerFactory getSubmitterSchedulerFactory() {
+    return new DelegatorFactory();
+  }
+  
+  @Test
+  public void executeDelegateTest() {
+    delegator.execute(DoNothingRunnable.instance());
+    delegator.submit(DoNothingRunnable.instance());
+    assertEquals(2, executor.tick());
+    assertEquals(0, scheduler.tick());
+  }
+  
+  @Test
+  public void scheduleDelegateTest() {
+    delegator.schedule(DoNothingRunnable.instance(), 10);
+    delegator.submitScheduled(DoNothingRunnable.instance(), 10);
+    delegator.scheduleAtFixedRate(DoNothingRunnable.instance(), 10, 100);
+    delegator.scheduleWithFixedDelay(DoNothingRunnable.instance(), 10, 100);
+    assertEquals(0, executor.advance(10));
+    assertEquals(4, scheduler.advance(10));
+  }
+
+  protected static class DelegatorFactory implements SubmitterSchedulerFactory {
+    private final PrioritySchedulerFactory schedulerFactory;
+    
+    public DelegatorFactory() {
+      schedulerFactory = new PrioritySchedulerFactory();
+    }
+    
+    @Override
+    public void shutdown() {
+      schedulerFactory.shutdown();
+    }
+
+    @Override
+    public SubmitterExecutor makeSubmitterExecutor(int poolSize, boolean prestartIfAvailable) {
+      return makeSubmitterScheduler(poolSize, prestartIfAvailable);
+    }
+
+    @Override
+    public SubmitterScheduler makeSubmitterScheduler(int poolSize, boolean prestartIfAvailable) {
+      SubmitterScheduler scheduler = schedulerFactory.makeSubmitterScheduler(poolSize, prestartIfAvailable);
+      
+      return new SchedulerExecutorDelegator(scheduler, scheduler);
+    }
+  }
+}
