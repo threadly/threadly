4e54ec9ceb95b7b486e26a2ecb31ebd7db82ad79
Mike Jensen <jent@threadly.org>
2016-09-23 13:23:01 -0600

    Javadoc fix

--- src/main/java/org/threadly/util/StringUtils.java
+++ src/main/java/org/threadly/util/StringUtils.java
@@ -13,7 +13,7 @@ public class StringUtils {
    * Constant to represent the line separator for the system (captured from 
    * System.getProperty("line.separator")).
    * 
-   * @deprecated Use System.lineSeperator() as an alternative (from JDK 1.7+)
+   * @deprecated Use System.lineSeparator() as an alternative (from JDK 1.7+)
    */
   @Deprecated
   public static final String NEW_LINE;
