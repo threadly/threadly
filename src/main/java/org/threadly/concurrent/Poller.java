3995763db83d810b9e476c8afb4d6242f9d09617
Mike Jensen <jent@threadly.org>
2017-01-02 10:38:50 -0700

    Added `Poller`
    This class, backed by a scheduling pool, can take a supplier of boolean (or a java.util.concurrent Future), and return a ListenableFuture for state change.
    
    Polling is a classic way of taking blocking or otherwise non-event driven things and making them event driven.  This class provides a simple and generic way to do this simple action.

---
+++ src/main/java/org/threadly/concurrent/Poller.java
@@ -0,0 +1,181 @@
+package org.threadly.concurrent;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.function.Supplier;
+
+import org.threadly.concurrent.future.FutureUtils;
+import org.threadly.concurrent.future.ImmediateResultListenableFuture;
+import org.threadly.concurrent.future.ListenableFuture;
+import org.threadly.concurrent.future.ListenableFutureAdapterTask;
+import org.threadly.concurrent.future.ListenableFutureTask;
+import org.threadly.concurrent.future.ListenableRunnableFuture;
+import org.threadly.concurrent.future.SettableListenableFuture;
+import org.threadly.concurrent.future.Watchdog;
+import org.threadly.util.Pair;
+
+/**
+ * Simple class for watching a condition and getting notified when a state has changed.  One handy 
+ * tool is the ability to transition java's {@link Future} into Threadly's much nicer 
+ * {@link ListenableFuture}.
+ * <p>
+ * The frequency at which this polls should be determined based off how cheap polling is, and how 
+ * many items will be polled on average.
+ * <p>
+ * If being allowed to garbage collect, this poller will continue to schedule itself as long as
+ * there are outstanding futures.  Once all have completed (from timeout or naturally), then this
+ * will stop scheduling itself to poll for updates.  Thus no explicit cleanup is needed.  As long 
+ * as the {@link Supplier}'s are quick/fast ({@link Future} conversions are always quick/fast), 
+ * it's most efficient to reuse the {@link Poller} instance.  But if you need dynamic 
+ * timeout's/max wait time you could construct a {@link Poller} and toss it away once you get the 
+ * returned {@link ListenableFuture} from it. 
+ */
+public class Poller {
+  private final Watchdog futureWatchdog;
+  private final PollRunner runner;
+
+  /**
+   * Construct a new poller which will run on the provided scheduler, and run at the specified
+   * frequency.
+   *
+   * @param scheduler Scheduler to run polling task on
+   * @param pollFrequency Time in milliseconds to wait between polling events
+   */
+  public Poller(SchedulerService scheduler, long pollFrequency) {
+    this(scheduler, pollFrequency, -1);
+  }
+
+  /**
+   * Construct a new poller which will run on the provided scheduler, and run at the specified
+   * frequency.
+   * <p>
+   * This constructor additionally allows you to specify the maximum time in millseconds we should
+   * wait for the condition to become true.  At this point if we are still not seeing our expected
+   * polling state, then the return future will be canceled.
+   *
+   * @param scheduler Scheduler to run polling task on
+   * @param pollFrequency Time in milliseconds to wait between polling events
+   * @param maxWaitTime Maximum time in milliseconds till returned futures should be canceled
+   */
+  public Poller(SchedulerService scheduler, long pollFrequency, long maxWaitTime) {
+    if (maxWaitTime > 0 && maxWaitTime != Long.MAX_VALUE) {
+      futureWatchdog = new Watchdog(scheduler, maxWaitTime, false);
+    } else {
+      futureWatchdog = null;
+    }
+    this.runner = new PollRunner(scheduler, pollFrequency);
+  }
+
+  /**
+   * Watch suppliers returned condition.  Once Supplier is witnessed in the {@code true} state, the
+   * returned future is completed.  Listeners and FutureCallback's executed on the returned future
+   * without a specified pool will run on the polling thread, and so should be kept to a minimum.
+   *
+   * @param p Supplier to provide state for when poll has completed as expected
+   * @return Future to complete once boolean state is witnessed as {@code true}
+   */
+  public ListenableFuture<?> watch(Supplier<Boolean> p) {
+    ListenableFuture<?> result = runner.watch(p);
+    if (futureWatchdog != null) {
+      futureWatchdog.watch(result);
+    }
+    return result;
+  }
+
+  /**
+   * Convert a conventional {@link Future} into a {@link ListenableFuture}.  As poller runs it 
+   * will check if the provided future has completed.  Once it does complete the returned future 
+   * will also complete in the exact same way.  Canceling the returned future will have NO impact 
+   * on the provided future (and thus the use with a timeout is not a concern to interrupting the 
+   * provided future).  Because this is only checked at poll intervals the returned future's 
+   * completion will be delayed by that polling delay.
+   * 
+   * @param <T> Type of object returned from future
+   * @param f Future to monitor for completetion
+   * @return ListenableFuture that will provide the result from the source future
+   */
+  @SuppressWarnings("unchecked")
+  public <T> ListenableFuture<T> watch(Future<? extends T> f) {
+    if ((futureWatchdog == null || f.isDone()) && f instanceof ListenableFuture) {
+      return (ListenableFuture<T>)f;
+    }
+    ListenableFuture<T> result = runner.watch(f);
+    if (futureWatchdog != null) {
+      futureWatchdog.watch(result);
+    }
+    return result;
+  }
+
+  /**
+   * Class which when run checks across a collection of polls, looking for ones which have 
+   * completed.  The runner reschedules itself as long as there is polls to be checked.
+   */
+  protected static class PollRunner extends ReschedulingOperation {
+    private final Collection<Pair<ListenableRunnableFuture<?>, Supplier<Boolean>>> polls =
+        new ConcurrentLinkedQueue<>();
+    
+    public PollRunner(SubmitterScheduler scheduler, long scheduleDelay) {
+      super(scheduler, scheduleDelay);
+    }
+
+    public ListenableFuture<?> watch(Supplier<Boolean> p) {
+      if (p.get()) {
+        return ImmediateResultListenableFuture.NULL_RESULT;
+      } else {
+        ListenableRunnableFuture<?> result =
+            new ListenableFutureTask<>(false, DoNothingRunnable.instance());
+        polls.add(new Pair<>(result, p));
+        signalToRun();
+        return result;
+      }
+    }
+
+    public <T> ListenableFuture<T> watch(Future<? extends T> f) {
+      if (f.isDone()) {  // optimized path for already complete futures if possible
+        if (f.isCancelled()) {
+          SettableListenableFuture<T> slf = new SettableListenableFuture<>();
+          slf.cancel(false);
+          return slf;
+        }
+        try {
+          return FutureUtils.immediateResultFuture(f.get());
+        } catch (InterruptedException e) {
+          // should not be possible
+          throw new RuntimeException(e);
+        } catch (ExecutionException e) {
+          // failure in getting result from future, transfer failure
+          return FutureUtils.immediateFailureFuture(e.getCause());
+        }
+      } else {
+        ListenableRunnableFuture<T> result = new ListenableFutureAdapterTask<T>(f);
+        polls.add(new Pair<>(result, () -> f.isDone()));
+        signalToRun();
+        return result;
+      }
+    }
+
+    @Override
+    public void run() {
+      Iterator<Pair<ListenableRunnableFuture<?>, Supplier<Boolean>>> it = polls.iterator();
+      boolean hasMore = false;
+      while (it.hasNext()) {
+        Pair<ListenableRunnableFuture<?>, Supplier<Boolean>> p = it.next();
+        if (p.getLeft().isDone()) {
+          it.remove();  // likely completed/canceled by Watchdog
+        } else if (p.getRight().get()) {
+          it.remove();
+          p.getLeft().run(); // mark as done
+        } else {
+          hasMore = true;
+        }
+      }
+
+      if (hasMore) {
+        signalToRun();
+      }
+    }
+  }
+}
---
+++ src/test/java/org/threadly/concurrent/PollerTest.java
@@ -0,0 +1,202 @@
+package org.threadly.concurrent;
+
+import static org.junit.Assert.*;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.threadly.concurrent.SingleThreadScheduler;
+import org.threadly.concurrent.future.FutureUtils;
+import org.threadly.concurrent.future.ListenableFuture;
+import org.threadly.concurrent.future.SettableListenableFuture;
+import org.threadly.test.concurrent.TestCondition;
+import org.threadly.test.concurrent.TestUtils;
+import org.threadly.test.concurrent.TestableScheduler;
+
+@SuppressWarnings("javadoc")
+public class PollerTest {
+  private static final int POLL_INTERVAL = 10_000;
+
+  private TestableScheduler scheduler;
+  private Poller poller;
+
+  @Before
+  public void setup() {
+    scheduler = new TestableScheduler();
+    poller = new Poller(scheduler, POLL_INTERVAL);
+  }
+
+  @After
+  public void cleanup() {
+    scheduler = null;
+    poller = null;
+  }
+
+  @Test
+  public void simplePollTest() {
+    AtomicBoolean pollState = new AtomicBoolean(false);
+
+    ListenableFuture<?> f = poller.watch(() -> pollState.get());
+    assertFalse(f.isDone());
+
+    assertEquals(1, scheduler.advance(POLL_INTERVAL));
+    assertFalse(f.isDone());
+
+    pollState.set(true);
+
+    assertEquals(1, scheduler.advance(POLL_INTERVAL));
+    assertTrue(f.isDone());
+
+    assertEquals(0, scheduler.advance(POLL_INTERVAL));
+  }
+
+  @Test
+  public void timeoutTest() {
+    poller = new Poller(new SingleThreadScheduler(), POLL_INTERVAL, 10);
+    ListenableFuture<?> f = poller.watch(() -> false);
+    TestUtils.sleep(10);
+
+    new TestCondition() {
+      @Override
+      public boolean get() {
+        return f.isDone() && f.isCancelled();
+      }
+    }.blockTillTrue();// will throw if does not become true
+  }
+  
+  @Test
+  public void watchListenableFutureWithExtendedResultTest() {
+    ListenableFuture<DoNothingRunnable> lf = FutureUtils.immediateResultFuture(DoNothingRunnable.instance());
+    ListenableFuture<Runnable> lfResult = poller.watch(lf);
+    
+    assertTrue(((ListenableFuture<?>)lfResult) == lf);
+  }
+  
+  @Test
+  public void watchAlreadyDoneFutureWithResultTest() throws InterruptedException, ExecutionException {
+    final Object objResult = new Object();
+    ListenableFuture<Object> lfResult = poller.watch(new AlreadyDoneFuture() {
+      @Override
+      public Object get() throws ExecutionException {
+        return objResult;
+      }
+
+      @Override
+      public Object get(long timeout, TimeUnit unit) throws ExecutionException {
+        return objResult;
+      }
+    });
+    
+    assertTrue(lfResult.isDone());
+    assertTrue(objResult == lfResult.get());
+  }
+  
+  @Test
+  public void watchAlreadyDoneFutureWithFailureTest() throws InterruptedException {
+    final Throwable rootCause = new Exception();
+    ListenableFuture<Object> lfResult = poller.watch(new AlreadyDoneFuture() {
+      @Override
+      public Object get() throws ExecutionException {
+        throw new ExecutionException(rootCause);
+      }
+
+      @Override
+      public Object get(long timeout, TimeUnit unit) throws ExecutionException {
+        throw new ExecutionException(rootCause);
+      }
+    });
+    
+    assertTrue(lfResult.isDone());
+    try {
+      lfResult.get();
+      fail("Exception should have thrown");
+    } catch (ExecutionException e) {
+      assertTrue(e.getCause() == rootCause);
+    }
+  }
+  
+  @Test
+  public void watchAlreadyCanceledFutureTest() {
+    Future<Object> canceledFuture = new FutureTask<>(() -> { return null; });
+    assertTrue(canceledFuture.cancel(true));
+    ListenableFuture<Object> lfResult = poller.watch(canceledFuture);
+    
+    assertTrue(lfResult.isDone());
+    assertTrue(lfResult.isCancelled());
+  }
+  
+  @Test
+  public void watchIncompleteFutureWithResultTest() throws InterruptedException, ExecutionException {
+    poller = new Poller(scheduler, POLL_INTERVAL, POLL_INTERVAL * 2);  // must have timeout to avoid just casting SLF
+    Object objResult = new Object();
+    SettableListenableFuture<Object> slf = new SettableListenableFuture<>();
+    ListenableFuture<Object> lfResult = poller.watch(slf);
+    
+    assertFalse(lfResult.isDone());
+    
+    slf.setResult(objResult);
+    assertEquals(1, scheduler.advance(POLL_INTERVAL));
+
+    assertTrue(lfResult.isDone());
+    assertTrue(objResult == lfResult.get());
+  }
+  
+  @Test
+  public void watchIncompleteFutureWithFailureTest() throws InterruptedException {
+    poller = new Poller(scheduler, POLL_INTERVAL, POLL_INTERVAL * 2);  // must have timeout to avoid just casting SLF
+    Throwable rootCause = new Exception();
+    SettableListenableFuture<Object> slf = new SettableListenableFuture<>();
+    ListenableFuture<Object> lfResult = poller.watch(slf);
+    
+    assertFalse(lfResult.isDone());
+    
+    slf.setFailure(rootCause);
+    assertEquals(1, scheduler.advance(POLL_INTERVAL));
+    
+    assertTrue(lfResult.isDone());
+    try {
+      lfResult.get();
+      fail("Exception should have thrown");
+    } catch (ExecutionException e) {
+      assertTrue(e.getCause() == rootCause);
+    }
+  }
+  
+  @Test
+  public void watchIncompleteCanceledFutureTest() {
+    poller = new Poller(scheduler, POLL_INTERVAL, POLL_INTERVAL * 2);  // must have timeout to avoid just casting SLF
+    SettableListenableFuture<Object> slf = new SettableListenableFuture<>();
+    ListenableFuture<Object> lfResult = poller.watch(slf);
+    
+    assertFalse(lfResult.isDone());
+    
+    slf.cancel(false);
+    assertEquals(1, scheduler.advance(POLL_INTERVAL));
+    
+    assertTrue(lfResult.isDone());
+    assertTrue(lfResult.isCancelled());
+  }
+  
+  private static abstract class AlreadyDoneFuture implements Future<Object> {
+    @Override
+    public boolean cancel(boolean mayInterruptIfRunning) {
+      return false;
+    }
+
+    @Override
+    public boolean isCancelled() {
+      return false;
+    }
+
+    @Override
+    public boolean isDone() {
+      return true;
+    }
+  }
+}
