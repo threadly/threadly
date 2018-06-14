78e239067863128fd949acccd0ccb647296ce58f
Mike Jensen <jent@threadly.org>
2018-06-14 09:13:57 -0600

    One limiter task for all keys
    This converts the design of having one limiter task per key, to one task for all keys.
    This reduces memory overhead, as well as likely computation overhead as lots of tasks are now managed by just one.
    The risk would be if there is so many keys that the limiter checker can't keep up.  That may occur at 10's or 100's of millions of keys.  But I suspect the old design likely would not have done well at that scale anyways (due to the larger memory overhead).

--- src/main/java/org/threadly/concurrent/wrapper/limiter/KeyedRateLimiterExecutor.java
+++ src/main/java/org/threadly/concurrent/wrapper/limiter/KeyedRateLimiterExecutor.java
@@ -1,5 +1,7 @@
 package org.threadly.concurrent.wrapper.limiter;
 
+import java.util.Iterator;
+import java.util.Map;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
@@ -7,6 +9,7 @@ import java.util.function.Function;
 import org.threadly.concurrent.AbstractSubmitterExecutor;
 import org.threadly.concurrent.DoNothingRunnable;
 import org.threadly.concurrent.PrioritySchedulerService;
+import org.threadly.concurrent.ReschedulingOperation;
 import org.threadly.concurrent.SubmitterExecutor;
 import org.threadly.concurrent.SubmitterScheduler;
 import org.threadly.concurrent.TaskPriority;
@@ -46,6 +49,7 @@ public class KeyedRateLimiterExecutor {
   protected final String subPoolName;
   protected final boolean addKeyToThreadName;
   protected final ConcurrentHashMap<Object, RateLimiterExecutor> currentLimiters;
+  protected final LimiterChecker limiterChecker;
   
   /**
    * Constructs a new key rate limiting executor.  Using sensible default options.
@@ -181,6 +185,7 @@ public class KeyedRateLimiterExecutor {
     this.subPoolName = StringUtils.nullToEmpty(subPoolName);
     this.addKeyToThreadName = addKeyToThreadName;
     this.currentLimiters = new ConcurrentHashMap<>(CONCURRENT_HASH_MAP_INITIAL_SIZE);
+    this.limiterChecker = new LimiterChecker(scheduler, LIMITER_IDLE_TIMEOUT / 2);
   }
   
   /**
@@ -424,9 +429,22 @@ public class KeyedRateLimiterExecutor {
         }
         v = new RateLimiterExecutor(threadNamedScheduler, permitsPerSecond, 
                                     maxScheduleDelayMillis, rejectedExecutionHandler);
-        // schedule task to check for removal later, should only be one task per limiter
-        limiterCheckerScheduler.schedule(new LimiterChecker(taskKey, v), LIMITER_IDLE_TIMEOUT);
+        limiterChecker.signalToRun();
       }
+      // TODO - I would like to improve this
+      //          This is awkward having to construct an Object[] just to get the result returned
+      //          Ideally we would avoid both that, and the lambda's that need to be passed in, but 
+      //          how to do that is not super obvious.  By applying the function inside the `compute` 
+      //          we are ensuring the operation is atomic compared to any removes that might want 
+      //          to happen.
+      //          Other ideas I have:
+      //            * Increase memory overhead by having the value be an Object[] of size 2
+      //                Index 0 would be the RateLimiterExecutor, 1 would be able to be used for the capture
+      //            * Extend RateLimiterExecutor and store a timestamp that is updated here in compute.
+      //                This is probably the simplest, but does not completely solve the problem
+      //                This would just ensure the timestamp is updated when looking to remove, but 
+      //                If the action took X milliseconds after the update, then it may still be removed.
+      //                Highly unlikely, but I prefer impossibility if we can
       capture[0] = c.apply(v);
       return v;
     });
@@ -482,31 +500,36 @@ public class KeyedRateLimiterExecutor {
   }
   
   /**
-   * Task which checks to see if a limiter has become idle.  If so it removes the limiter in a 
-   * thread safe way.  If the limiter is still active then it will schedule itself to check again 
-   * later.
+   * Task which checks over all limiters to see if any should be expired / removed.
    * 
-   * @since 4.7.0
+   * @since 5.25
    */
-  protected class LimiterChecker implements Runnable {
-    public final Object taskKey;
-    public final RateLimiterExecutor limiter;
-    
-    public LimiterChecker(Object taskKey, RateLimiterExecutor limiter) {
-      this.taskKey = taskKey;
-      this.limiter = limiter;
+  protected class LimiterChecker extends ReschedulingOperation {
+    protected LimiterChecker(SubmitterScheduler scheduler, long scheduleDelay) {
+      super(scheduler, scheduleDelay);
     }
 
     @Override
     public void run() {
-      currentLimiters.computeIfPresent(taskKey, (k, v) -> {
-        if (Clock.lastKnownForwardProgressingMillis() - v.getLastScheduleTime() < LIMITER_IDLE_TIMEOUT) {
-          limiterCheckerScheduler.schedule(this, LIMITER_IDLE_TIMEOUT / 2);
-          return v;
-        } else {
-          return null;  // remove
+      Iterator<Map.Entry<Object, RateLimiterExecutor>> it = currentLimiters.entrySet().iterator();
+      long now = Clock.lastKnownForwardProgressingMillis();
+      while (it.hasNext()) {
+        Map.Entry<Object, RateLimiterExecutor> e = it.next();
+        if (now - e.getValue().getLastScheduleTime() > LIMITER_IDLE_TIMEOUT) {
+          // if optimistic check above failed, we must remove in `compute` to ensure
+          // no tasks are being submitted while we are removing the limiter
+          currentLimiters.computeIfPresent(e.getKey(), (k, v) -> {
+            if (now - v.getLastScheduleTime() > LIMITER_IDLE_TIMEOUT) {
+              return null;
+            } else {
+              return v;
+            }
+          });
         }
-      });
+      }
+      if (! currentLimiters.isEmpty()) {
+        signalToRun();
+      }
     }
   }
 }
