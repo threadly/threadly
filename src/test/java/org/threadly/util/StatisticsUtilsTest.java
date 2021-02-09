211b13dbe6e2397a0acea444fdd2e56dbb3d58fd
Mike Jensen <jent@threadly.org>
2021-02-08 20:00:10 -0700

    StatisticsUtils getMax / getMin implementation
    Using the power of generics we can easily add these.

--- src/test/java/org/threadly/util/StatisticsUtilsTest.java
+++ src/test/java/org/threadly/util/StatisticsUtilsTest.java
@@ -23,6 +23,46 @@ public class StatisticsUtilsTest extends ThreadlyTester {
   }
   
   @Test
+  public void maxTest() {
+    assertEquals(2, StatisticsUtils.getMax(Arrays.asList(new Integer[]{ 2 })), 0);
+    assertEquals(-2, StatisticsUtils.getMax(Arrays.asList(new Integer[]{ -2 })), 0);
+    assertEquals(2, StatisticsUtils.getMax(Arrays.asList(new Integer[]{ 1, 2 })), 0);
+    assertEquals(3, StatisticsUtils.getMax(Arrays.asList(new Integer[]{ 3, 1, 2 })), 0);
+    assertEquals(2, StatisticsUtils.getMax(Arrays.asList(new Integer[]{ -3, 1, 2 })), 0);
+
+    assertEquals(2., StatisticsUtils.getMax(Arrays.asList(new Double[]{ 2. })), 0);
+    assertEquals(-2., StatisticsUtils.getMax(Arrays.asList(new Double[]{ -2. })), 0);
+    assertEquals(2., StatisticsUtils.getMax(Arrays.asList(new Double[]{ 1., 2. })), 0);
+    assertEquals(3.1, StatisticsUtils.getMax(Arrays.asList(new Double[]{ 3.1, 3.0, 1., 2. })), 0);
+    assertEquals(2., StatisticsUtils.getMax(Arrays.asList(new Double[]{ -3., 1., 2. })), 0);
+  }
+  
+  @Test (expected = IllegalArgumentException.class)
+  public void maxFail() {
+    StatisticsUtils.getMax(Collections.<Long>emptyList());
+  }
+  
+  @Test
+  public void minTest() {
+    assertEquals(2, StatisticsUtils.getMin(Arrays.asList(new Integer[]{ 2 })), 0);
+    assertEquals(-2, StatisticsUtils.getMin(Arrays.asList(new Integer[]{ -2 })), 0);
+    assertEquals(1, StatisticsUtils.getMin(Arrays.asList(new Integer[]{ 2, 1 })), 0);
+    assertEquals(1, StatisticsUtils.getMin(Arrays.asList(new Integer[]{ 3, 1, 2 })), 0);
+    assertEquals(-4, StatisticsUtils.getMin(Arrays.asList(new Integer[]{ -3, 1, -4 })), 0);
+
+    assertEquals(2., StatisticsUtils.getMin(Arrays.asList(new Double[]{ 2. })), 0);
+    assertEquals(-2., StatisticsUtils.getMin(Arrays.asList(new Double[]{ -2. })), 0);
+    assertEquals(1., StatisticsUtils.getMin(Arrays.asList(new Double[]{ 1., 2. })), 0);
+    assertEquals(1., StatisticsUtils.getMin(Arrays.asList(new Double[]{ 3., 1., 2. })), 0);
+    assertEquals(-3.1, StatisticsUtils.getMin(Arrays.asList(new Double[]{ -3., 1., 2., -3.1 })), 0);
+  }
+  
+  @Test (expected = IllegalArgumentException.class)
+  public void minFail() {
+    StatisticsUtils.getMin(Collections.<Long>emptyList());
+  }
+  
+  @Test
   public void percentileTest() {
     Map<Double, Integer> pResult = 
         StatisticsUtils.getPercentiles(Arrays.asList(new Integer[]{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }), 
